9717b132411cc3dea44916b208903de3ebb2ff2d
4kazch <119107799+4kazch@users.noreply.github.com>
2023-09-25 11:45:16 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Student 
+{
+	public static void main(String args[])
+	{
+		String Name, Sex, Branch;
+		int Age;
+		
+		Name = "John";
+		Sex ="Male";
+		Branch = "CS";
+		Age = 19;
+		
+		System.out.println("Name = " + Name);
+		System.out.println("Sex =" + Sex);
+		System.out.println("Branch = " + Branch);
+		System.out.println("Age = " + Age);
+	}
+}
---
+++ addition.java
@@ -0,0 +1,9 @@
+public class addition
+{
+	public static void main(String[] args)
+	{
+		int x = 900;
+		int y = 11;
+		System.out.println(x+y);
+	}
+}
